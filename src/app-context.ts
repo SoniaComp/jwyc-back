398656e421254e702c19f94442617aa44e01d7ba
Sonia Park <sonia@Sonias-MacBook-Air.local>
2023-05-01 00:14:41 +0900

    add model and router

--- src/app-context.ts
+++ src/app-context.ts
@@ -1,5 +1,69 @@
 import sequelize from "@/repo/sequelize-ctx";
+import Sequelize from "sequelize";
 
 const ctx = {};
 
+ctx["sequelize"] = sequelize;
+ctx["Sequelize"] = Sequelize;
+
+ctx["congregation"] = (sequelize, Sequelize) => {
+    const Congregation = sequelize.define("congregation", {
+        id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        no: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+    })
+    return Congregation
+}
+ctx["attendance"] = (sequelize, Sequelize) => {
+    const Attendance = sequelize.define("attendance", {
+        id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        congregation: { // TODO: FOREIGN KEY
+            type: Sequelize.INTEGER,
+            allowNull: false
+            // uniqueKey
+        },
+        names: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        count: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        create_dt: {
+            type: Sequelize.DATE,
+            allowNull: false
+        },
+        update_dt: {
+            type: Sequelize.DATE,
+            allowNull: false
+        },
+        is_scan: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            default: false
+        },
+        scan_qr_dt: {
+            type: Sequelize.DATE,
+            allowNull: false,
+        }
+    })
+    return Attendance
+}
 export default ctx;
--- src/router/user-router.ts
+++ src/router/user-router.ts
@@ -19,5 +19,51 @@ router.get('/debug', handler(async (req, res) => {
   });
 }));
 
+// 회중 목록 조회 (Validation)
+router.get('/api/congregation', handler(async (req, res) => {
+  res.json({
+    headers: req.headers,
+    method: req.method,
+    path: req.path,
+    queries: req.query,
+    body: req.body,
+    params: req.params,
+  });
+}));
 
+// 참여자 등록
+router.post('/api/attendance', handler(async (req, res) => {
+  res.json({
+    headers: req.headers,
+    method: req.method,
+    path: req.path,
+    queries: req.query,
+    body: req.body,
+    params: req.params,
+  });
+}));
+
+// 참여자 수정
+router.put('/api/attendance', handler(async (req, res) => {
+  res.json({
+    headers: req.headers,
+    method: req.method,
+    path: req.path,
+    queries: req.query,
+    body: req.body,
+    params: req.params,
+  });
+}));
+
+// 스캔 여부 등록
+router.post('/api/attendance-scan', handler(async (req, res) => {
+  res.json({
+    headers: req.headers,
+    method: req.method,
+    path: req.path,
+    queries: req.query,
+    body: req.body,
+    params: req.params,
+  });
+}));
 export default router;
